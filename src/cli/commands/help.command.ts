483035e3a62e5e2b3200a0d2be54c6083953509f
Artem Trikashnyi <artema.luch@gmail.com>
2024-01-13 20:21:24 +0200

    Добавит chalk и раскрасит вывод в консоли

--- src/cli/commands/help.command.ts
+++ src/cli/commands/help.command.ts
@@ -1,4 +1,5 @@
 import { Command } from './command.interface.js';
+import chalk from 'chalk';
 
 export class HelpCommand implements Command {
   public getName(): string {
@@ -7,14 +8,14 @@ export class HelpCommand implements Command {
 
   public async execute(..._parameters: string[]): Promise<void> {
     console.info(`
-        Программа для подготовки данных для REST API сервера.
-        Пример:
-            cli.js --<command> [--arguments]
-        Команды:
-            --version:                   # выводит номер версии
-            --help:                      # печатает этот текст
-            --import <path>:             # импортирует данные из TSV
-            --generate <n> <path> <url>  # генерирует произвольное количество тестовых данных
+        ${chalk.bgBlue('Программа для подготовки данных для REST API сервера.')}
+        ${chalk.bgYellow('Пример:')}
+            ${chalk.green('cli.js --<command> [--arguments]')}
+        ${chalk.bgYellow('Команды:')}
+            ${chalk.green('--version:')}                   ${chalk.magenta('# выводит номер версии')}
+            ${chalk.green('--help:')}                      ${chalk.magenta('# печатает этот текст')}
+            ${chalk.green('--import <path>:')}             ${chalk.magenta('# импортирует данные из TSV')}
+            ${chalk.green('--generate <n> <path> <url>')}  ${chalk.magenta('# генерирует произвольное количество тестовых данных')}
     `);
   }
 }
