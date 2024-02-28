251b57c8fd705b43e59fc33109460e12b2a3c99f
Artem Trikashnyi <artema.luch@gmail.com>
2024-02-28 20:03:10 +0200

    Добавит дефолтное изображение для аватара

--- src/shared/modules/user/dto/create-user.messages.ts
+++ src/shared/modules/user/dto/create-user.messages.ts
@@ -6,9 +6,6 @@ export const CreateUserMessages = {
   email: {
     invalidFormat: 'email must be a valid address'
   },
-  avatar: {
-    invalidFormat: 'avatar is required',
-  },
   password: {
     invalidFormat: 'password is required',
     lengthField: 'min length for password is 6, max is 12'
